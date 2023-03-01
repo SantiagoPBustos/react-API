78bc4907f052cc83658fd8b192fd8ddae351e16f
SantiagoPBustos <david.pina@uptc.edu.co>
2023-03-01 14:35:19 -0500

    other commite

--- src/helpers/helpHTTP.js
+++ src/helpers/helpHTTP.js
@@ -1,19 +1,23 @@
 export const HelperHTTP = () => {
-    const customFetch = (endpoint, options) =>{
+    const customFetch = (endpoint, options) => {
         const defaultHeader = {
             accept: "application/json",
         };
 
         const controller = new AbortController();
         options.signal = controller.signal;
+
+        options.method = options.method || "GET";
+        options.headers = options.headers ? { ...defaultHeader, ...options.headers } : defaultHeader;
+
     }
 
-    const get = () =>{}
-    const post = () =>{}
-    const put = () =>{}
-    const del = () =>{}
+    const get = () => { }
+    const post = () => { }
+    const put = () => { }
+    const del = () => { }
 
-    return{
+    return {
         get,
         post,
         put,
